2b014691373774da18b38b0238bfcc6138e82edd
siraja36 <siraja36@gmail.com>
2023-12-30 17:49:55 +0300

    Update collect_normal_traffic.py

--- generate_normal_traffic/collect_normal_traffic.py
+++ generate_normal_traffic/collect_normal_traffic.py
@@ -4,17 +4,20 @@ from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.lib import hub
 from datetime import datetime
+import os
 
 class CollectTrainingStatsApp(switch.SimpleSwitch13):
     def __init__(self, *args, **kwargs):
         super(CollectTrainingStatsApp, self).__init__(*args, **kwargs)
         self.datapaths = {}
         self.monitor_thread = hub.spawn(self.monitor)
-        file0 = open("FlowStatsfile.csv", "w")
-        file0.write('timestamp,datapath_id,flow_id,ip_src,tp_src,ip_dst,tp_dst,ip_proto,icmp_code,icmp_type,'
-                     'flow_duration_sec,flow_duration_nsec,idle_timeout,hard_timeout,flags,packet_count,byte_count,'
-                     'packet_count_per_second,packet_count_per_nsecond,byte_count_per_second,byte_count_per_nsecond,label\n')
-        file0.close()
+        self.log_file_path = "FlowStatsfile.csv"
+        self.initialize_log_file()
+
+    def initialize_log_file(self):
+        if not os.path.exists(self.log_file_path):
+            with open(self.log_file_path, "w") as file0:
+                file0.write('timestamp,datapath_id,flow_id,ip_src,tp_src,ip_dst,tp_dst,ip_proto,icmp_code,icmp_type,flow_duration_sec,flow_duration_nsec,idle_timeout,hard_timeout,flags,packet_count,byte_count,packet_count_per_second,packet_count_per_nsecond,byte_count_per_second,byte_count_per_nsecond,label\n')
 
     @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
     def state_change_handler(self, ev):
@@ -42,53 +45,60 @@ class CollectTrainingStatsApp(switch.SimpleSwitch13):
 
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
-        timestamp = datetime.now()
-        timestamp = timestamp.timestamp()
+        timestamp = datetime.now().timestamp()
         icmp_code = -1
         icmp_type = -1
         tp_src = 0
         tp_dst = 0
 
-        file0 = open("FlowStatsfile.csv", "a+")
-        body = ev.msg.body
-        for stat in sorted([flow for flow in body if flow.priority == 1],
-                           key=lambda flow: (flow.match['eth_type'], flow.match['ipv4_src'],
-                                            flow.match['ipv4_dst'], flow.match['ip_proto'])):
-            ip_src = stat.match['ipv4_src']
-            ip_dst = stat.match['ipv4_dst']
-            ip_proto = stat.match['ip_proto']
-            if stat.match['ip_proto'] == 1:
-                icmp_code = stat.match['icmpv4_code']
-                icmp_type = stat.match['icmpv4_type']
-            elif stat.match['ip_proto'] == 6:
-                tp_src = stat.match['tcp_src']
-                tp_dst = stat.match['tcp_dst']
-            elif stat.match['ip_proto'] == 17:
-                tp_src = stat.match['udp_src']
-                tp_dst = stat.match['udp_dst']
-            flow_id = str(ip_src) + str(tp_src) + str(ip_dst) + str(tp_dst) + str(ip_proto)
-            try:
-                packet_count_per_second = stat.packet_count / stat.duration_sec
-                packet_count_per_nsecond = stat.packet_count / stat.duration_nsec
-            except:
-                packet_count_per_second = 0
-                packet_count_per_nsecond = 0
-            try:
-                byte_count_per_second = stat.byte_count / stat.duration_sec
-                byte_count_per_nsecond = stat.byte_count / stat.duration_nsec
-            except:
-                byte_count_per_second = 0
-                byte_count_per_nsecond = 0
-            file0.write("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n"
-                         .format(timestamp, ev.msg.datapath.id, flow_id, ip_src, tp_src, ip_dst, tp_dst,
-                                 stat.match['ip_proto'], icmp_code, icmp_type,
-                                 stat.duration_sec, stat.duration_nsec,
-                                 stat.idle_timeout, stat.hard_timeout,
-                                 stat.flags, stat.packet_count, stat.byte_count,
-                                 packet_count_per_second, packet_count_per_nsecond,
-                                 byte_count_per_second, byte_count_per_nsecond, 0))
-        file0.close()
+        with open(self.log_file_path, "a+") as file0:
+            body = ev.msg.body
+            for stat in sorted([flow for flow in body if flow.priority == 1],
+                               key=lambda flow: (
+                               flow.match['eth_type'], flow.match['ipv4_src'], flow.match['ipv4_dst'],
+                               flow.match['ip_proto'])):
+
+                ip_src = stat.match['ipv4_src']
+                ip_dst = stat.match['ipv4_dst']
+                ip_proto = stat.match['ip_proto']
+
+                if stat.match['ip_proto'] == 1:
+                    icmp_code = stat.match['icmpv4_code']
+                    icmp_type = stat.match['icmpv4_type']
+                elif stat.match['ip_proto'] == 6:
+                    tp_src = stat.match['tcp_src']
+                    tp_dst = stat.match['tcp_dst']
+                elif stat.match['ip_proto'] == 17:
+                    tp_src = stat.match['udp_src']
+                    tp_dst = stat.match['udp_dst']
+
+                flow_id = str(ip_src) + str(tp_src) + str(ip_dst) + str(tp_dst) + str(ip_proto)
+
+                try:
+                    packet_count_per_second = stat.packet_count / stat.duration_sec
+                    packet_count_per_nsecond = stat.packet_count / stat.duration_nsec
+                except ZeroDivisionError:
+                    packet_count_per_second = 0
+                    packet_count_per_nsecond = 0
+
+                try:
+                    byte_count_per_second = stat.byte_count / stat.duration_sec
+                    byte_count_per_nsecond = stat.byte_count / stat.duration_nsec
+                except ZeroDivisionError:
+                    byte_count_per_second = 0
+                    byte_count_per_nsecond = 0
+
+                file0.write("{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n"
+                             .format(timestamp, ev.msg.datapath.id, flow_id, ip_src, tp_src, ip_dst, tp_dst,
+                                     stat.match['ip_proto'], icmp_code, icmp_type,
+                                     stat.duration_sec, stat.duration_nsec,
+                                     stat.idle_timeout, stat.hard_timeout,
+                                     stat.flags, stat.packet_count, stat.byte_count,
+                                     packet_count_per_second, packet_count_per_nsecond,
+                                     byte_count_per_second, byte_count_per_nsecond, 0))
+
 
 if __name__ == "__main__":
     from ryu.cmd import manager
+
     manager.main()
